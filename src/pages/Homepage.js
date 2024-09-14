6f74815ac0d1e201009b05b820aec836b2b4f467
guptaprakhar2612 <guptaprakhar2612@gmail.com>
2024-09-14 22:45:26 +0530

    toast notification added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,17 @@
 import React,{useState} from 'react'
 import {v4 as uuidV4} from 'uuid';
+import toast from 'react-hot-toast';
 
 
 const HomePage = ()=>{
     const [roomId,setRoomId]=useState('');
+    const [username,setUsername]=useState('');
     const createNewRoom = (e) =>{
         e.preventDefault();
         const id = uuidV4();
         setRoomId(id);
+        toast.success('Created a new room');
+
         
     };
     return <div className='homePageWrapper'>
@@ -15,8 +19,18 @@ const HomePage = ()=>{
             <img src = "/logo192.png" alt="change the logo "/>
             <h4 className='mainLable'>Paste ROOM ID</h4>
             <div className='inputGroup'>
-                <input type="text" className="inputBox" placeholder = "USERNAME"/>
-                <input type="text" className="inputBox" placeholder = "ROOM ID" value = {roomId}/>
+                <input type="text"
+                 className="inputBox" 
+                 placeholder = "USERNAME"
+                 onChange = {(e)=> setUsername(e.target.value)}
+                 value = {username}/>
+                <input type="text"
+                 className="inputBox" 
+                 placeholder = "ROOM ID" 
+                 value = {roomId} 
+                 onChange = {(e)=> setRoomId(e.target.value)}/>
+                 
+                
                 <button className="btn joinBtn">Join</button>
                 <span className='createInfo'>
                     if you dont have an invite then create &nbsp;
