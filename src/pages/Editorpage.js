3c7e53d9f6746a4c39963add1dab55e203a7c384
guptaprakhar2612 <guptaprakhar2612@gmail.com>
2024-09-15 16:11:19 +0530

    editor page

--- src/pages/Editorpage.js
+++ src/pages/Editorpage.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Client from '../components/Client';
+
 
 const EditorPage = ()=>{
+    const [clients,setClients]= useState([{socketId: 1,username: 'Prakhar'},
+    {socketId: 2,username: 'Pranay'},
+    ]);
     return(
-        <div>Editor page</div>
+        <div className = 'mainwrap'>
+            <div className = 'left'>
+                <div className = 'left-top'>
+                    <img className = 'logo' src= '/logo192.png' alt = 'logo'/>
+                </div>
+            <h3> Connected </h3>
+            </div>
+            <div className='clientList'>
+                {clients.map((client)=>(
+                    <Client key={client.socketId} username = {client.username} />
+                ))}
+
+            </div>
+            <button className="btn copyBtn" >
+                    Copy ROOM ID
+                </button>
+                <button className="btn leaveBtn" >
+                    Leave
+                </button>
+
+            <div className = 'middle'>
+            
+                 </div>
+            <div className = 'right'></div>
+            <div className = 'bottom'></div>
+            
+        </div>
     )
 }
 
