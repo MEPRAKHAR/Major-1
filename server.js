1a0075fd0ecd5fe6199d73f7eabb936abe9f7468
Pranay Mayal <pranaymayal17@gmail.com>
2024-11-26 17:12:24 +0530

    add C++ language to the compiling

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 //server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 //for over wifi
